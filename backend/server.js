6d6ba1996929e423ff04defec41a9f02938f8f49
Gopala Khore <gopala@teesta.co>
2022-02-07 14:15:16 +0530

    Something changed

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,35 @@
  const express = require('express');
  const cors = require('cors');
  const mongoose = require('mongoose');
- const exerciseRoute = require('./routes/exercises');
- const userRoute = require('./routes/users');
+
 
  require('dotenv').config();
 
  const app = express();
  const port = process.env.PORT || 3000;
 
- app.use(cors);
+ app.use(cors());
  app.use(express.json());
+
  const uri = process.env.ATLAS_URI;
- mongoose.connect(uri, { useNewUrlParser: true,  });
+
+ mongoose.connect(uri, { useNewUrlParser: true});
+
  const connection = mongoose.connection;
+
  connection.once('open', () => {
      console.log("Connection with mongo established succesfully");
- } );
+ })
+
+ const exerciseRoute = require('./routes/exercises');
+ const userRoute = require('./routes/users');
 
  app.use('/exercises', exerciseRoute);
  app.use('/users', userRoute);
+
  app.listen(port, () => {
      console.log(`Server started at port: ${port}`)
- })
+ });
+
+
+
